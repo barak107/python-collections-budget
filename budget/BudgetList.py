7e4fc63281cbf26767172576ecc3d37deb96b3fd
barak107 <77495628+barak107@users.noreply.github.com>
2021-04-14 14:44:51 +0300

    add iter

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 
 class BudgetList:
     
@@ -20,6 +21,17 @@ class BudgetList:
             self.overages.append(item)
             self.sum_overages += item
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+    
+    def __next__(self):
+        try:
+            return next(self.iter_e)
+        except StopIteration as stop:
+            return next(self.iter_o)
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
@@ -27,6 +39,12 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
     print('The count of all expenses: '+ str(len(myBudgetList)))
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values,color=['green','red','blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
 if __name__ == "__main__":
     main()
